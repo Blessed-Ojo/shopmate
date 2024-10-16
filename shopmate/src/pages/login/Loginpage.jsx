02ebda4ff5fe588d0e9cc31b215a6665f1261aa6
Blessed <ojoblessed533@gmail.com>
2024-10-16 19:58:32 -0400

    "Added react-icons dependency, updated Loginpage and Signuppage components with new functionality, and created corresponding CSS files"

--- shopmate/src/pages/login/Loginpage.jsx
+++ shopmate/src/pages/login/Loginpage.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import { useState } from "react";
+import "./loginpage.css";
+import { FaShoppingCart } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const Loginpage = () => {
+  const [formData, setFromData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+  const handleChanage = (e) => {
+    setFromData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+    const user = users.find(
+      (u) =>
+        u.username === formData.username &&
+        u.email === formData.email &&
+        u.password === formData.password
+    );
+    if (user) {
+      onlogin(user);
+      navigate("/");
+    } else {
+      setError("Invalid credentials.!");
+    }
+  };
   return (
-    <div>Loginpage</div>
-  )
-}
+    <div className="Loginpage">
+      <h3>
+        Shop
+        <span>
+          Mate
+          <FaShoppingCart />
+        </span>
+      </h3>
+      <h2 className="login">Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          value={formData.username}
+          onChange={handleChanage}
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          value={formData.email}
+          onChange={handleChanage}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          value={formData.password}
+          onChange={handleChanage}
+          required
+        />
+        {error && <p>{error}</p>}
+        <button type="submit" className="signbtn">
+          login
+        </button>
+      </form>
+      <h4 className="links">
+        <a href="signup">signup</a>
+      </h4>
+    </div>
+  );
+};
 
-export default Loginpage
+export default Loginpage;
--- shopmate/src/pages/sginup/Signuppage.jsx
+++ shopmate/src/pages/sginup/Signuppage.jsx
@@ -1,29 +1,59 @@
-import'./signup.css'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { FaShoppingCart } from 'react-icons/fa';
+import './signup.css'
 
 const Signuppage = () => {
+  const [formData, setFormData] = useState({ username: '', password: '' , email:'' });// is like taking the form to a new form (setFormData)
+
+  const navigate = useNavigate(); // after signup it will take you to the cart
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value }) // this place everything in the right way like fill the correct key in the right box
+  };
+
+  const handleSubmit =(e) =>{ // to prevent the submit btn from working when the inputs are not fill
+    e.preventDefault(); // stop the page from reloading
+    const users = JSON.parse(localStorage.getItem('users')) || []; // to check if a user as acc if not we signup on a new pages for new users
+    users.push(formData); // for putting new users
+    localStorage.setItem('users',JSON.stringify(users)); // to store the user imform in the browser storage so you don have to re fill them again
+    navigate('/login') //after a new user signup it takes u to a login page to login
+  };
+
+
   return (
     <div className='Signuppage'>
-       <h3>Shop<span>Mate</span></h3>
-        <h2 className='signup'>signUp</h2>
-        <form>
-          <input 
-          type="text" 
-           name="username"
-           placeholder="username"
-           value
-            onChange
-            required
-          />
-          <input
+      <h3>Shop<span>Mate<FaShoppingCart/></span></h3>
+      <h2 className='signup'>signUp</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="name"
+          name="username"
+          placeholder="username"
+          value={formData.username}
+          onChange={handleChange}
+          required
+        />
+        <input type="Email" 
+          name='email'
+          placeholder='email'
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        <input
           type="password"
           name="password"
           placeholder="Password"
-          value
-          onChange
+          value={formData.password}
+          onChange={handleChange}
           required
-          />
-          <button type='submit' className='signbtn'>SignUp</button>
-        </form>
+        />
+        <button type='submit' className='signbtn'>SignUp</button>
+      </form>
+       <div className="link">
+       <h4 className='links'><a href="login">login</a></h4>
+       </div>
     </div>
   )
 }
